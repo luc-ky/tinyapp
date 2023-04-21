e07c677ab39d295415a938e1074de34367fe42ad
David Luc <luc.ky@me.com>
2023-04-21 01:47:01 -0400

    Refactored getUserByEmail function and moved to helpers.js file.

---
+++ helpers.js
@@ -0,0 +1,11 @@
+// email lookup helper function
+const getUserByEmail = (email, users) => {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return user;
+    }
+  }
+  return undefined;
+};
+
+module.exports = getUserByEmail;
