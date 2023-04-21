817607e1929f1152cdf1d7ecc115d4cd72d75b99
David Luc <luc.ky@me.com>
2023-04-21 01:08:54 -0400

    Added encrypted cookies using cookie-session. Removed cookie-parser.

--- express_server.js
+++ express_server.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const bcrypt = require("bcryptjs");
+
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
-const generateRandomString = () => {
-  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return Array.from({ length: 6 }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
-};
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 
 const urlDatabase = {
   b6UTxQ: {
@@ -24,6 +25,16 @@ const urlDatabase = {
 };
 
 const users = {};
+
+const generateRandomString = () => {
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return Array.from({ length: 6 }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
+};
+
+// return urls that belong to user
+const urlsForUser = (id, urlDatabase) => Object.fromEntries(
+  Object.entries(urlDatabase).filter((entry) => entry[1].userID === id)
+);
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -41,13 +52,8 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// return urls that belong to user
-const urlsForUser = (id, urlDatabase) => Object.fromEntries(
-  Object.entries(urlDatabase).filter(([_, data]) => data.userID === id)
-);
-
 app.get("/urls", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const templateVars = {
     urls: urlsForUser(userID, urlDatabase),
     user: users[userID]
@@ -56,7 +62,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const templateVars = {
     urls: urlDatabase,
     user: users[userID]
@@ -69,7 +75,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   if (!userID) {
     res.status(403).send("Please log in to edit or view your URL");
     return;
@@ -87,7 +93,7 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   if (!userID) {
     res.status(401).send("Sorry, you must be logged in to shorten URLs.");
     return;
@@ -110,7 +116,7 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const shortURL = req.params.id;
   if (!Object.keys(urlsForUser(userID, urlDatabase)).includes(shortURL)) {
     res.status(401).send("Sorry, you do not have permission to view or edit this URL");
@@ -121,7 +127,7 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const shortURL = req.params.id;
   if (!Object.keys(urlsForUser(userID, urlDatabase)).includes(shortURL)) {
     res.status(401).send("Sorry, you do not have permission to view or edit this URL");
@@ -134,14 +140,13 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const bcrypt = require("bcryptjs");
 
   if (!getUserByEmail(email, users)) {
     res.status(403).send("Email cannot be found");
   } else {
     for (let userID in users) {
       if (email === users[`${userID}`]["email"] && bcrypt.compareSync(password, users[`${userID}`]["password"])) {
-        res.cookie('user_id', userID);
+        req.session.user_id = userID;
         res.redirect('/urls');
       } else {
         res.status(403).send("Incorrect password");
@@ -151,12 +156,12 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect('/login');
 });
 
 app.get("/register", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const templateVars = {
     user: users[userID]
   };
@@ -188,13 +193,13 @@ app.post("/register", (req, res) => {
       password: hashedPassword,
     };
     console.log(users);
-    res.cookie('user_id', newUserID);
+    req.session.user_id = newUserID;
   }
   res.redirect('/urls');
 });
 
 app.get("/login", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const templateVars = {
     user: users[userID]
   };
