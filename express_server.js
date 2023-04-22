ee88f4cb70642464b1f39475462077d49fbbb17f
David Luc <luc.ky@me.com>
2023-04-22 03:00:14 -0400

    Moved databases to db.js. Cleaned up some code and added some comments.

--- express_server.js
+++ express_server.js
@@ -1,14 +1,15 @@
 const { generateRandomString, getUserByEmail, urlsForUser } = require('./helpers.js');
+const { users, urlDatabase } = require('./db.js');
 
-const express = require("express");
+const express = require('express');
 const cookieSession = require('cookie-session');
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 const methodOverride = require('method-override');
 
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({
@@ -17,43 +18,40 @@ app.use(cookieSession({
 }));
 app.use(methodOverride('_method'));
 
-const urlDatabase = {};
-const users = {};
-
 app.listen(PORT, () => {
-  console.log(`TinyApp listening on port ${PORT}!`);
+  console.log(`TinyApp is listening on port ${PORT}!`);
 });
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const user = users[req.session.user_id];
-  if (user) {
+  if (user) { // if user is logged in
     res.redirect('/urls');
   } else {
     res.redirect('/login');
   }
 });
 
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   const userID = req.session.user_id;
-  if (userID) {
-    const templateVars = {
+  if (userID) { 
+    let templateVars = {
       urls: urlsForUser(userID, urlDatabase),
       user: users[userID]
     };
-    res.render("urls_index", templateVars);
+    res.render('urls_index', templateVars);
   } else {
     let templateVars = {
-      user: users[req.session.user_id],
+      user: users[userID],
       error: 'Sorry, please login or register to access this website',
     };
-    res.render('error', templateVars);
+    res.render('error', templateVars); // pass error message to template
     return;
   }
 });
 
-app.get("/urls/new", (req, res) => {
+app.get('/urls/new', (req, res) => {
   const userID = req.session.user_id;
-  const templateVars = {
+  let templateVars = {
     urls: urlDatabase,
     user: users[userID]
   };
@@ -61,11 +59,12 @@ app.get("/urls/new", (req, res) => {
     res.redirect('/login');
     return;
   }
-  res.render("urls_new", templateVars);
+  res.render('urls_new', templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
+app.get('/urls/:id', (req, res) => {
   const userID = req.session.user_id;
+  const shortURL = req.params.id;
   if (!userID) {
     let templateVars = {
       user: users[req.session.user_id],
@@ -73,8 +72,14 @@ app.get("/urls/:id", (req, res) => {
     };
     res.render('error', templateVars);
     return;
-  }
-  if (!Object.keys(urlsForUser(userID, urlDatabase)).includes(req.params.id)) {
+  } else if (userID === urlDatabase[shortURL].userID) { // if URL belongs to user
+    let templateVars = {
+      id: req.params.id,
+      longURL: urlDatabase[req.params.id].longURL,
+      user: users[userID]
+    };
+    res.render('urls_show', templateVars);
+  } else {
     let templateVars = {
       user: users[req.session.user_id],
       error: 'Sorry, you do not have permission to view or edit this URL',
@@ -82,15 +87,9 @@ app.get("/urls/:id", (req, res) => {
     res.render('error', templateVars);
     return;
   }
-  const templateVars = {
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id].longURL,
-    user: users[userID]
-  };
-  res.render("urls_show", templateVars);
 });
 
-app.get("/u/:id", (req, res) => {
+app.get('/u/:id', (req, res) => {
   const url = urlDatabase[req.params.id];
   if (url) {
     const longURL = url.longURL;
@@ -105,31 +104,31 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-app.get("/login", (req, res) => {
+app.get('/login', (req, res) => {
   const userID = req.session.user_id;
-  const templateVars = {
+  let templateVars = {
     user: users[userID]
   };
   if (userID) {
     res.redirect('/urls');
     return;
   }
-  res.render("login", templateVars);
+  res.render('login', templateVars);
 });
 
-app.get("/register", (req, res) => {
+app.get('/register', (req, res) => {
   const userID = req.session.user_id;
-  const templateVars = {
+  let templateVars = {
     user: users[userID]
   };
   if (userID) {
     res.redirect('/urls');
     return;
   }
-  res.render("register", templateVars);
+  res.render('register', templateVars);
 });
 
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   const userID = req.session.user_id;
   if (!userID) {
     let templateVars = {
@@ -147,7 +146,7 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortID}`);
 });
 
-app.post("/login", (req, res) => {
+app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
@@ -160,7 +159,7 @@ app.post("/login", (req, res) => {
     return;
   } else {
     for (let userID in users) {
-      if (email === users[`${userID}`]["email"] && bcrypt.compareSync(password, users[`${userID}`]["password"])) {
+      if (email === users[`${userID}`]['email'] && bcrypt.compareSync(password, users[`${userID}`]['password'])) {
         req.session.user_id = userID;
         res.redirect('/urls');
       } else {
@@ -175,15 +174,15 @@ app.post("/login", (req, res) => {
   }
 });
 
-app.post("/logout", (req, res) => {
+app.post('/logout', (req, res) => {
   req.session = null;
   res.redirect('/login');
 });
 
-app.post("/register", (req, res) => {
+app.post('/register', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const bcrypt = require("bcryptjs");
+  const bcrypt = require('bcryptjs');
   const hashedPassword = bcrypt.hashSync(password, 10);
 
   if (!email || !password) { // if email or password are empty
@@ -212,10 +211,13 @@ app.post("/register", (req, res) => {
   res.redirect('/urls');
 });
 
-app.put("/urls/:id", (req, res) => {
+app.put('/urls/:id', (req, res) => {
   const userID = req.session.user_id;
   const shortURL = req.params.id;
-  if (!Object.keys(urlsForUser(userID, urlDatabase)).includes(shortURL)) { // if URL doesn't belong to user
+  if (userID === urlDatabase[shortURL].userID) { // if URL belongs to user
+    urlDatabase[shortURL].longURL = req.body.newURL;
+    res.redirect('/urls');
+  } else {
     let templateVars = {
       user: users[req.session.user_id],
       error: 'Sorry, you do not have permission to edit this URL',
@@ -223,14 +225,15 @@ app.put("/urls/:id", (req, res) => {
     res.render('error', templateVars);
     return;
   }
-  urlDatabase[shortURL].longURL = req.body.newURL;
-  res.redirect('/urls');
 });
 
-app.delete("/urls/:id/delete", (req, res) => {
+app.delete('/urls/:id/delete', (req, res) => {
   const userID = req.session.user_id;
   const shortURL = req.params.id;
-  if (!Object.keys(urlsForUser(userID, urlDatabase)).includes(shortURL)) { // if URL doesn't belong to user
+  if (userID === urlDatabase[shortURL].userID) { 
+    delete urlDatabase[shortURL];
+    res.redirect('/urls');
+  } else {
     let templateVars = {
       user: users[req.session.user_id],
       error: 'Sorry, you do not have permission to delete this URL',
@@ -238,6 +241,4 @@ app.delete("/urls/:id/delete", (req, res) => {
     res.render('error', templateVars);
     return;
   }
-  delete urlDatabase[shortURL];
-  res.redirect('/urls');
 });
--- helpers.js
+++ helpers.js
@@ -1,12 +1,13 @@
-// generate random 6 length string from all alphanumberic characters
+// generate random 6 length string from all possible alphanumberic characters
 const generateRandomString = () => {
   const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  // generate an array of length 6 and fill it with possible characters, then concatenate into a string and return value
   return Array.from({ length: 6 }, () => characters[Math.floor(Math.random() * characters.length)]).join('');
 };
 
-// email lookup helper function
+// checks if a user is registered in the database using their email
 const getUserByEmail = (email, users) => {
-  for (const user in users) {
+  for (let user in users) {
     if (users[user].email === email) {
       return user;
     }
@@ -15,8 +16,16 @@ const getUserByEmail = (email, users) => {
 };
 
 // return urls that belong to user
-const urlsForUser = (id, urlDatabase) => Object.fromEntries(
-  Object.entries(urlDatabase).filter((entry) => entry[1].userID === id)
-);
+const urlsForUser = (userID, urlDatabase) => {
+  let userUrls = {};
+  // loop through each URL in the database
+  for (let url in urlDatabase) {
+    // if the URL belongs to the given user, add it to the userUrls object
+    if (urlDatabase[url].userID=== userID) {
+      userUrls[url] = urlDatabase[url];
+    }
+  }
+  return userUrls;
+};
 
 module.exports = { generateRandomString, getUserByEmail, urlsForUser };
